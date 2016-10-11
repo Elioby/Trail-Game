ed9acd313e0be7d6145736062db67a9a71151bbc
Joshua Hornsby <hornsbyjm@cardiff.ac.uk>
2016-10-11 20:07:53 +0100

    Added all the cities to the cities list

--- cities.py
+++ cities.py
@@ -3,6 +3,7 @@
 # This file contains data about the cities you travel through
 
 # TODO: Should this be a dictionary itself, or just a list?
+# TODO: add descriptions for each of the cities
 cities = {
 	
 	"Los Angeles": {
@@ -11,12 +12,63 @@ cities = {
 
 		# The distance, in miles, from Los Angeles (the start)
 		"distance_from_start": 0,
-
-
 	},
+	
+	"Phoenix": {
+		# A brief (1 sentence?) description of the city
+		"description": "",
 
-	# TODO: Copy and paste the above dictonary and fill in for the remaining 7 cities.
-	#			Presume that New York, which is the end of the trail, is 3000 miles away.
+		# The distance, in miles, from Los Angeles (the start)
+		"distance_from_start": 300,
+	},
+	
+	"Albuquerque": {
+		# A brief (1 sentence?) description of the city
+		"description": "",
+
+		# The distance, in miles, from Los Angeles (the start)
+		"distance_from_start": 700,
+	},
+	
+	"Oklahoma City": {
+		# A brief (1 sentence?) description of the city
+		"description": "",
+
+		# The distance, in miles, from Los Angeles (the start)
+		"distance_from_start": 1300,
+	},
+	
+	"St Louis": {
+		# A brief (1 sentence?) description of the city
+		"description": "",
+
+		# The distance, in miles, from Los Angeles (the start)
+		"distance_from_start": 1800,
+	},
+	
+	"Indianapolis": {
+		# A brief (1 sentence?) description of the city
+		"description": "",
+
+		# The distance, in miles, from Los Angeles (the start)
+		"distance_from_start": 2000,
+	},
+	
+	"Columbus": {
+		# A brief (1 sentence?) description of the city
+		"description": "",
+
+		# The distance, in miles, from Los Angeles (the start)
+		"distance_from_start": 2200,
+	},
+	
+	"Pittsburgh": {
+		# A brief (1 sentence?) description of the city
+		"description": "",
+
+		# The distance, in miles, from Los Angeles (the start)
+		"distance_from_start": 2600,
+	},
 
 	"New York": {
 		# A brief (1 sentence?) description of the city
@@ -24,8 +76,6 @@ cities = {
 
 		# The distance, in miles, from Los Angeles (the start)
 		"distance_from_start": 3000,
-
-
 	},
 
 }
